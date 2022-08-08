64410d80b7451627e6e65305a5af48daa8712c7d
SergeiDegtyarev <degtiarev94@yandex.ru>
2022-08-08 16:26:20 +0400

    1

---
+++ src/main/java/Cat.java
@@ -0,0 +1,19 @@
+public class Cat {
+
+    private String name;
+
+    public Cat() {
+
+    }
+
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+}
